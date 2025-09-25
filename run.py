a084dd71b4cd0763d68c14e619341c3bfb5bcb0c
Allzer <dshapkarin@list.ru>
2025-09-25 12:09:04 +0300

    убрал кеш

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-from fastapi import FastAPI
 import uvicorn
-
-app = FastAPI()
+from src.main import app
 
 if __name__ == '__main__':
     uvicorn.run('run:app', host='0.0.0.0', port=8000, reload=True)
